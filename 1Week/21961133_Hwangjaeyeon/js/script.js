9a742c47854b26d07b9c0ae9744b29167b227b83
Namehwang <54699767+Namehwang@users.noreply.github.com>
2020-09-03 17:42:04 +0900

    Update script.js
    21961133 황재연

--- 1Week/21961133_Hwangjaeyeon/js/script.js
+++ 1Week/21961133_Hwangjaeyeon/js/script.js
@@ -1,6 +1,7 @@
 (function($){
     $(document).ready(function(){
-        alert("경고");
-        console.log("안녕하세욧");
+        alert("21961133 황재연 입니다");
+        console.log("2학기 수업 잘 부탁드립니다! 쉽진 않겠지만 열심히 수업 따라가서 전학기 보다 좋은 결과물 만들어 제출하겠습니다."
+        );
     });
 })(jQuery);
