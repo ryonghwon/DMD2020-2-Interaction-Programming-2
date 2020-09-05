8a2770116ac90ac7d939f65fce15361ce5697fb3
makaronn21 <56422160+makaronn21@users.noreply.github.com>
2020-09-05 17:50:51 +0900

    1week

--- 1Week/21961127 HanJeongMin/js/script.js
+++ 1Week/21961127 HanJeongMin/js/script.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function(){
-        console.log("안녕하세요");
-        alert("경고창");
+        console.log("안녕하세요 21961127 한정민입니다! (인터넷...너무 많이 끊겨서 아쉬웠습니다...)");
+        alert("21961127/한정민");
     });
 })(jQuery);
