7815edb4412cc38f74cbec3bdb3aad60f990ede0
billyaemi <54699658+billyaemi@users.noreply.github.com>
2020-09-10 02:09:29 +0900

    .

--- 1Week/21961065_WonHeeYun/js/script.js
+++ 1Week/21961065_WonHeeYun/js/script.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function(){
-        console.log("안녕하세요");
-        alert("경고창입니다");
+        console.log("안녕하세요 잘부탁드립니다.");
+        alert("21961065/원희윤");
     });
 })(jQuery);
