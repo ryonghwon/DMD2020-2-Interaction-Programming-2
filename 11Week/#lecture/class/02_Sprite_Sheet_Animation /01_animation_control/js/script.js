822768fd77fdacb2d19cc173e37aaa8ff7750660
Kim Yong Won <rh@102labs.com>
2020-11-12 04:12:45 +0900

    11주차 수업 준비.

---
+++ 11Week/#lecture/class/02_Sprite_Sheet_Animation /01_animation_control/js/script.js
@@ -0,0 +1,13 @@
+console.log("Script Load");
+(function($){
+    $(document).ready(function() {
+        console.log('jQuery Ready');
+
+        var _max = 48;
+        var _col = 7;
+        var _row = 7;
+        var _imgW = 1008;
+        var _imgH = 1008;
+        
+    });
+})(jQuery);
